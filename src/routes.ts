c069bedc70d4d7cd86319bfe5c41ef1b1ef9cac5
Prof. Renato Oliveira <162147229+profRenatoOliveira1@users.noreply.github.com>
2026-03-14 13:51:57 -0300

    feat: criando endpoints da entidade aluno

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router, type Request, type Response } from "express";
+import AlunoController from "./controller/AlunoController.js";
 
 const router = Router();
 
@@ -10,5 +11,12 @@ router.get('/', (req: Request, res: Response) => {
             status(200).
             json(`Aplicação online. Timestamp: ${new Date()}`);
 });
+
+// Endpoints Aluno
+router.get('/api/alunos', AlunoController.todos);
+router.get('/api/alunos/:id', AlunoController.aluno);
+router.post('/api/alunos', AlunoController.cadastrar);
+router.delete('/api/alunos/:id', AlunoController.remover);
+router.put('/api/alunos/:id', AlunoController.atualizar);
 
 export { router }
