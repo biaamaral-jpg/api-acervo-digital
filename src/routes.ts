107538c2f412f2aed017d1c0712177700f65697e
Prof. Renato Oliveira <162147229+profRenatoOliveira1@users.noreply.github.com>
2026-03-14 15:31:47 -0300

    feat: criando endpoints de livro

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router, type Request, type Response } from "express";
 import AlunoController from "./controller/AlunoController.js";
+import LivroController from "./controller/LivroController.js";
 
 const router = Router();
 
@@ -18,5 +19,12 @@ router.get('/api/alunos/:id', AlunoController.aluno);
 router.post('/api/alunos', AlunoController.cadastrar);
 router.delete('/api/alunos/:id', AlunoController.remover);
 router.put('/api/alunos/:id', AlunoController.atualizar);
+
+// Endpoints Livro
+router.get('/api/livros', LivroController.todos);
+router.get('/api/livros/:id', LivroController.livro);
+router.post('/api/livros', LivroController.cadastrar);
+router.delete('/api/livros/:id', LivroController.remover);
+router.put('/api/livros/:id', LivroController.atualizar);
 
 export { router }
